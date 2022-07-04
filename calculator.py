01c5cfb19b26cceab6be3971309ad946a84d4ac0
Ahmed Hajeyah <hajeyah@gmail.com>
2022-07-04 18:46:27 +0300

    Solved

--- calculator.py
+++ calculator.py
@@ -2,8 +2,25 @@
 def main():
 	#write your code here
 	pass
-	
+	FirstNumber= float(input("Enter first number: "))
+	SecondNumber= float(input("Enter second number: "))
+	operation= input("Choose the operation (+, -, /, *): ")
+	if operation == "+":
+		sum= FirstNumber + SecondNumber
+		print(f"The answer is : {sum}")
+	elif operation == "-":
+		sub= FirstNumber - SecondNumber
+		print(f"The answer is : {sub}")
+	elif operation == "/":
+		div= FirstNumber / SecondNumber
+		print(f"The answer is : {div}")
+	elif operation == "*":
+		mul= FirstNumber * SecondNumber
+		print(f"The answer is : {mul}")
 
+
+
+	
 
 
 if __name__ == '__main__':
